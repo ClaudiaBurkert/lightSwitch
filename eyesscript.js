3f7cbc992560b1354932d4b122ff8f11a38107bb
Claudia Burkert <burkert.claudia@gmail.com>
2024-08-15 11:19:14 +0200

    first commit

---
+++ eyesscript.js
@@ -0,0 +1,28 @@
+title = document.getElementById('website_title'); 
+
+console.log(title.innerHTML);
+
+title.innerHTML = "Lichtschalter"; 
+
+
+
+function lightSwitch() {
+    let body =  document.getElementsByTagName('body')[0];
+    let button = document.getElementById('light'); 
+    let eyes = document.getElementById('eyes'); 
+
+   body.classList.toggle('light'); 
+
+    if(body.classList.contains('light')) {
+        button.innerText = 'Licht an'; 
+        eyes.classList.remove('d-none');
+
+    } else {
+        button.innerText = 'Licht aus'; 
+        eyes.classList.add('d-none');
+    }
+}
+
+// document.getElementById('test-input').setAttribute('type', 'text');
+
+document.getElementById('test-input').value = 456;
